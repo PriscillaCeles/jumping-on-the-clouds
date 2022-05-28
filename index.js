d6f8dff15e91828b8260b44e62c7e3f217bfd4b9
Priscilla Celes Lima <pri.c.maciel@hotmail.com>
2022-05-28 13:40:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const list = [0, 0, 1, 0, 0, 1, 0]
 
 function jumpingOnClouds(){
   let cloudsCount = 0
+  
   for (let i = 0; i < list.length; i++) {
     if (list[i+2] === 0){
       cloudsCount++
@@ -16,3 +17,19 @@ function jumpingOnClouds(){
 const contagem = jumpingOnClouds()
 
 console.log(`São necessários ${contagem} movimentos`)
+
+//* CODE FOR SITE */
+function jumpingOnClouds(c) {
+  let cloudsCount = 0
+  
+  for (let i = 0; i < c.length; i++) {
+    if (c[i+2] === 0){
+      cloudsCount++
+      i++
+    } else if (c[i+1] === 0){
+      cloudsCount++
+    } 
+  }
+  return cloudsCount
+}
+
